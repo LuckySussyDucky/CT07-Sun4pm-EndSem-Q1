6aca43d9a82b6dc3447729c74d8c2e82f9f076ed
LuckySussyDucky <isaac.david.lee.05@gmail.com>
2025-10-26 08:16:16 +0000

    done

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,17 @@
+let bird
+let background, birdImg;
 
+function preload(){
+    background = loadImage("assets/background.png");
+    birdImg = loadImage("assets/bird.png");
+}
+
+function setup(){
+    new Canvas(400, 600);
+    bird = new Sprite(200, 300, 40, 30, "static");
+    bird.img = birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,16 @@
+let square;
 
+function setup(){
+    new Canvas(400, 600);
+    world.gravity.y = 10;
+
+    square = new Sprite(200, 300, 25, 25, "dynamic");
+}
+
+function draw(){
+    background("grey");
+
+    if(mouse.presses() ){
+        square.vel.y = -5;
+    }
+}
